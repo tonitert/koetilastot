d9ad4ffd9b0210111206e3ceb483ed6f0eaecf30
t0nero <20608826+t0nero@users.noreply.github.com>
2023-05-28 20:18:56 +0300

    feat: add full screen

--- src/components/containers/GraphBox.tsx
+++ src/components/containers/GraphBox.tsx
@@ -1,5 +1,6 @@
 import ViewSwitcher from "../ViewSwitcher";
 import React from "react";
+import {BsArrowsFullscreen, BsX} from "react-icons/bs";
 
 interface GraphBoxProps {
     titleText: string,
@@ -15,15 +16,35 @@ export default class GraphBox extends React.Component {
 
     props: GraphBoxProps
 
-    render() {
-        return (
-            <div className="card graph-card">
-                <h3 style={{margin: "10px"}}>{this.props.titleText}</h3>
-                {this.props.views.length === 1 ?
-                    this.props.views[0].element :
-                    <ViewSwitcher views={this.props.views} currentView={this.props.currentView}/>}
+    state: {
+        fullscreen: boolean
+    } = {
+        fullscreen: false
+    }
 
+    ref = React.createRef<HTMLDivElement>()
+
+    private onFullscreenClick() {
+        this.setState({
+            fullscreen: !this.state.fullscreen
+        })
+    }
+
+    render() {
+        let elem = <div ref={this.ref} className={`card graph-card ${this.state.fullscreen ? "fullscreen" : ""}`}>
+            <h3 style={{margin: "10px"}}>{this.props.titleText}<BsArrowsFullscreen onClick={() => this.onFullscreenClick()} style={{float: "right"}}/></h3>
+            {this.props.views.length === 1 ?
+                this.props.views[0].element :
+                <ViewSwitcher views={this.props.views} currentView={this.props.currentView}/>}
+
+        </div>
+        if(this.state.fullscreen) {
+            elem = <div className={"fullscreen-backdrop"}>
+                {elem}
             </div>
+        }
+        return (
+            elem
         );
     }
 }
